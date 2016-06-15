e7a9ffba49800a79ec6a61c3073617d880c70afc
mgfeller <mgfeller@hsr.ch>
2016-06-15 16:17:21 +0200

    update url listen

--- Vorlesung/Mongosse/index.js
+++ Vorlesung/Mongosse/index.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/demo');
+mongoose.connect('mongodb://localhost/demo2');
 
 var User = require('./User.js');
 
--- Vorlesung/Session/index.js
+++ Vorlesung/Session/index.js
@@ -18,5 +18,6 @@ app.get("/*", function(req,res){
     res.end(`Dein ${req.session.counter} besuch`);
 });
 
-app.listen(3000);
-
+app.listen(3000, function(){
+    console.log('listening on http://localhost:3000');
+});
