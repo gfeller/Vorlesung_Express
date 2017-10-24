ab769c5c503eaff93a3fb839cb029480867549bc
Michael Gfeller <mgfeller@hsr.ch>
2017-10-24 15:26:57 +0200

    update cors example

---
+++ Vorlesung/CORS/index2.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname));
+
+app.listen(3001, function () {
+    console.log('Example app listening on port http://lvh.me:3001/');
+});
