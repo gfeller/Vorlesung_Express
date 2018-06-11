df06ec11b8ce28173048a12124e41b6a059cb43a
Michael Gfeller <mgfeller@hsr.ch>
2018-06-11 13:54:06 +0200

    update cookie demo

--- Vorlesung/Cookie/index.js
+++ Vorlesung/Cookie/index.js
@@ -1,19 +1,18 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 app.use(require("cookie-parser")());
 
-
-app.get("/cookieDemo/*", function(req,res){
+app.get("/cookieDemo/*", function (req, res) {
     console.log(JSON.stringify(req.cookies));
     res.cookie("url", req.url);
-    if(req.cookies.url)
-    {
-        res.end("dein letzter besuch: "+ req.cookies.url)
+    if (req.cookies.url) {
+        res.end("dein letzter besuch: " + req.cookies.url)
     }
-    else{
+    else {
         res.end("Dein erster besuch?!")
     }
 });
 
-app.listen(3000);
-
+app.listen(3000, function () {
+    console.log('listening on http://localhost:3000');
+});
