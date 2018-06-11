3c5028306a0a90df7a64d191f37cbeb259b3f758
Michael Gfeller <mgfeller@hsr.ch>
2018-06-11 14:15:49 +0200

    update socketio

--- Vorlesung/SocketIO/index.js
+++ Vorlesung/SocketIO/index.js
@@ -1,17 +1,17 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendfile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
 });
 
-http.listen(3000, function(){
-  console.log('listening on http://localhost:3000');
+http.listen(3000, function () {
+    console.log('listening on http://localhost:3000');
 });
