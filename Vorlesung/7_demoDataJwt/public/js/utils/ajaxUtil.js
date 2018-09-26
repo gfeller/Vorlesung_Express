9db63b21292e83c02236168b10ff8a073758ab38
Michael Gfeller <mgfeller@hsr.ch>
2018-09-26 18:00:01 +0200

    update syntax

--- Vorlesung/7_demoDataJwt/public/js/utils/ajaxUtil.js
+++ Vorlesung/7_demoDataJwt/public/js/utils/ajaxUtil.js
@@ -1,7 +1,11 @@
 class Util {
     ajax(method, url, data, headers) {
-        const fetchHeaders = new Headers({'content-type': 'application/json', ...(headers || {})})
-        return fetch(url, {method: method, headers: fetchHeaders, body: JSON.stringify(data)}).then(x => {
+        const fetchHeaders = new Headers({'content-type': 'application/json', ...(headers || {})});
+
+        return fetch(url, {
+            method: method,
+            headers: fetchHeaders, body: JSON.stringify(data)
+        }).then(x => {
             return x.json();
         });
     }
