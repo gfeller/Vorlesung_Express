9a9cdde730522ea2ea1a6715e35e3c41f729320b
Michael Gfeller <michael.gfeller@ost.ch>
2021-06-17 14:19:53 +0200

    updated

--- Vorlesung/JSON/json.js
+++ Vorlesung/JSON/json.js
@@ -1,25 +1,27 @@
-var myObject = { a : '1234', b : function(){ return this.a}, c : 1234, d : new Date(), e : [4,5,6,7] };
-console.log(1, JSON.stringify( myObject ));
+var myObject = {
+    a: '1234', b: function () {
+        return this.a
+    }, c: 1234, d: new Date(), e: [4, 5, 6, 7]
+};
+console.log(1, JSON.stringify(myObject));
 
 var string = '{"a":"1234","c":1234,"d":"2015-06-12T05:45:02.270Z","e":[4,5,6,7]}';
 var object = JSON.parse(string);
-console.log(2,object.d);
-console.log(3,new Date(object.d).getDay());
+console.log(2, object.d);
+console.log(3, new Date(object.d).getDay());
 
 
-var undef = JSON.stringify({"undef" : undefined});
+var undef = JSON.stringify({"undef": undefined});
 console.log(4, undef);
 
 try {
-    var nok2 = JSON.parse("{'a':'1234'}");
-}
-catch(err)
-{
-    console.log(6, err);
+    JSON.parse("{'a':'1234'}");
+} catch (err) {
+    console.log(5, err);
 }
 
 
 //nicht ideal:
-JSON.stringify([1,2,4,5]);
+JSON.stringify([1, 2, 4, 5]);
 //besser:
-JSON.stringify({elements : [1,2,4,5]});
+JSON.stringify({elements: [1, 2, 4, 5]});
