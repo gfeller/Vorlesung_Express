897541b075ae3b40c1225ad888c8c429b794cd99
Michael Gfeller <michael.gfeller@ost.ch>
2021-10-05 15:43:44 +0200

    fixes

--- Vorlesung/7_demoDataJwt/routes/index-routes.js
+++ Vorlesung/7_demoDataJwt/routes/index-routes.js
@@ -3,6 +3,6 @@ import express from 'express';
 const router = express.Router();
 import {indexController} from '../controller/index-controller.js';
 
-router.post("/login", indexController.login);
+router.post("/login", indexController.lgin);
 
 export const indexRoutes = router;
