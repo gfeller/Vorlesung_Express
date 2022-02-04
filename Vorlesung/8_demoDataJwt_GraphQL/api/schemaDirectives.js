f6a168957fb3d72f6c43cf4f41ccc81df84c95dc
Michael Gfeller <michael.gfeller@ost.ch>
2022-02-04 12:30:32 +0100

    removed comment

--- Vorlesung/8_demoDataJwt_GraphQL/api/schemaDirectives.js
+++ Vorlesung/8_demoDataJwt_GraphQL/api/schemaDirectives.js
@@ -4,66 +4,7 @@ import graphql from "graphql";
 
 const {defaultFieldResolver} = graphql;
 import { mapSchema, getDirective, MapperKind } from '@graphql-tools/utils'
-
-/*
-
-const {ApolloServer, gql, SchemaDirectiveVisitor} = apollo;
-//import * as tools from "@graphql-tools/schema";
-//const { SchemaDirectiveVisitor} = tools; // TODO migrate to 3.0
-
-export class AuthDirective extends SchemaDirectiveVisitor {
-    visitObject(type) {
-        this.ensureFieldsWrapped(type);
-        type._requiresAuth = true;
-        type._requiresAdmin = this.args.isAdmin;
-    }
-
-    // Visitor methods for nested types like fields and arguments
-    // also receive a details object that provides information about
-    // the parent and grandparent types.
-    visitFieldDefinition(field, details) {
-        this.ensureFieldsWrapped(details.objectType);
-        field._requiresAuth = true;
-        field._requiresAdmin = this.args.isAdmin;
-    }
-
-    ensureFieldsWrapped(objectType) {
-        // Mark the GraphQLObjectType object to avoid re-wrapping:
-        if (objectType._authFieldsWrapped) return;
-        objectType._authFieldsWrapped = true;
-
-        const fields = objectType.getFields();
-
-        Object.keys(fields).forEach(fieldName => {
-            const field = fields[fieldName];
-            const {resolve = defaultFieldResolver} = field;
-            field.resolve = async function (...args) {
-                // Get the required Role from the field first, falling back
-                // to the objectType if no Role is required by the field:
-
-                const requiresAuth = field._requiresAuth || objectType._requiresAuth;
-                const requiredAdmin = field._requiresAdmin || objectType._requiresAdmin;
-
-                const user = args[2].user;
-
-                if (requiresAuth) {
-                    if (!user) {
-                        throw new Error("not authorized");
-                    }
-
-                    if (requiredAdmin) {
-                        if (!user.isAdmin) {
-                            throw new Error("not authorized");
-                        }
-                    }
-                }
-                return resolve.apply(this, args);
-            };
-        });
-    }
-}
-
-*/
+ 
 function authDirective(directiveName, getUserFn) {
     const typeDirectiveArgumentMaps = {}
     return {
