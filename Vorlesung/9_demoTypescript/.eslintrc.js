5438de821ae2ae98f33c130eb770d934fe4a2f7b
Michael Gfeller <michael.gfeller@ost.ch>
2022-05-24 15:13:45 +0200

    ts version

--- Vorlesung/9_demoTypescript/.eslintrc.js
+++ Vorlesung/9_demoTypescript/.eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-namespace": "off",
         "max-len": "off",
-        "@typescript-eslint/explicit-module-boundary-types": "off"
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-non-null-assertion": "off"
     }
 };
--- Vorlesung/9_demoTypescript/tests/index-controller.test.ts
+++ Vorlesung/9_demoTypescript/tests/index-controller.test.ts
@@ -14,7 +14,7 @@ process.env.NODE_ENV = "testing"
 dotenv.config({path: `.env-testing`});
 
 // load app after env
-const app = (await import('../app.js')).app;
+import {app} from '../app';
 
 describe('INDEX Controller', () => {
     it('should return login page if not logged in', async () => {
@@ -22,7 +22,7 @@ describe('INDEX Controller', () => {
         response.should.have.status(200);
 
         const dom = new jsdom.JSDOM(response.text);
-        dom.window.document.querySelector("form").should.have.attribute('action', '/login')
+        dom.window.document.querySelector("form")!.should.have.attribute('action', '/login')
         expect(dom.window.document.querySelector("form")).have.attribute('action', '/login')
     });
 
@@ -31,7 +31,7 @@ describe('INDEX Controller', () => {
         const response = await server.post('/login').type('form').send({email: 'michael.gfeller@ost.ch', pwd: '1234'});
         const dom = new jsdom.JSDOM(response.text);
 
-        dom.window.document.querySelector("form[action='/orders']").should.not.be.null;
+        dom.window.document.querySelector("form[action='/orders']")!.should.not.be.null;
         server.close();
     });
 });
