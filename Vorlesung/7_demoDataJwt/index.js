668111c6b112c3e481f9d55ab61a6acd1356e517
Michael Gfeller <michael.gfeller@ost.ch>
2022-12-15 07:44:44 +0100

    port

--- Vorlesung/7_demoDataJwt/index.js
+++ Vorlesung/7_demoDataJwt/index.js
@@ -7,7 +7,7 @@ dotenv.config({ path: `.env${process.env.NODE_ENV ? `-${process.env.NODE_ENV}` :
 const app = (await import('./app.js')).app;
 
 const hostname = '127.0.0.1';
-const port = 3001;
+const port = process.env.PORT || 3001;
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
