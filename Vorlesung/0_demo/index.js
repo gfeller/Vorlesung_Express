2b51bb616cdc2009cc03b711a134c82a5683b507
Michael Gfeller <michael.gfeller@ost.ch>
2025-02-18 07:56:39 +0100

    demo_0

--- Vorlesung/0_demo/index.js
+++ Vorlesung/0_demo/index.js
@@ -3,8 +3,14 @@ const app = express();
 const router = express.Router();
 
 
-
-
 const hostname = '127.0.0.1';
 const port = 3001;
-app.listen(port, hostname, () => {  console.log(`Server running at http://${hostname}:${port}/`); });
+
+app.listen(port, hostname, (error) => {
+    if(error){
+        console.error(error);
+    }
+    else {
+        console.log(`Server running at http://${hostname}:${port}/`);
+    }
+});
