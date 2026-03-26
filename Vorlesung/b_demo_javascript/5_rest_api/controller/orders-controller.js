29361a4a4893fd2fdeee72a54412997222dd1626
Michael Gfeller <michael.gfeller@ost.ch>
2026-03-26 09:21:58 +0100

    schema util

--- Vorlesung/b_demo_javascript/5_rest_api/controller/orders-controller.js
+++ Vorlesung/b_demo_javascript/5_rest_api/controller/orders-controller.js
@@ -4,7 +4,7 @@ import {securityService} from '../services/security-service.js'
 export class OrdersController {
 
     getOrders = async (req, res) => {
-        res.json((await orderStore.all(securityService.currentUser(req)) || []))
+        res.json((await orderStore.all(securityService.currentUser(req)) || [])) // Note: Should only return own orders
     };
 
     createPizza = async (req, res) => {
@@ -12,7 +12,7 @@ export class OrdersController {
     };
 
     showOrder = async (req, res) => {
-        res.json(await orderStore.get(req.params.id, securityService.currentUser(req)));
+        res.json(await orderStore.get(req.params.id, securityService.currentUser(req))); // Note: Should return 402 if permission failed. Error is detected by unit tests
     };
 
     deleteOrder = async (req, res) => {
