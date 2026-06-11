6667cc4f4805330091afdc46f6f72996085799fe
Michael Gfeller <michael.gfeller@ost.ch>
2026-06-11 11:18:40 +0200

    schema util

--- Vorlesung/c_demo_typescript/controller/index-controller.ts
+++ Vorlesung/c_demo_typescript/controller/index-controller.ts
@@ -25,8 +25,8 @@ export class IndexController {
             res.status(400).send({error: error});
             return;
         }
- /*
-      
+        /*
+        Variante 2: mit error handling
         const data = SchemaUtil.parseOrThrow(LoginSchema, req.body)
 
         if (await userStore.authenticate(data)) {
@@ -34,7 +34,7 @@ export class IndexController {
             res.json(token);
         } else {
             res.status(401).json(false);
-        }*/ 
+        }*/
     }
 }
 
